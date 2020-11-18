1307aaecb5770b0cd073584195d17c19aabd4718
Вова Судаков <shopvitiaz@rambler.ru>
2020-11-18 15:05:19 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber = 12;
+            Console.WriteLine("Компьютер Загадал Число ОТ 1-20.Попробуйте его угадать");
+            int userNumber = int.Parse(Console.ReadLine());
+            if(userNumber == secretNumber)
+            {
+                Console.WriteLine("Вы УГАДАЛИ!!!");
+            }
+
+            if(userNumber<secretNumber)
+            {
+                Console.WriteLine("Секретное число больше");
+            }
+
+            if(userNumber>secretNumber)
+            {
+                Console.WriteLine("Секретное число меньше");
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp5/dfs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int secretNumber = 12;
+            Console.WriteLine("Компьютер Загадал Число ОТ 1-20.Попробуйте его угадать");
+           for(int i =0; i < 5;)
+            {
+                int userNumber = int.Parse(Console.ReadLine());
+                if (userNumber == secretNumber)
+                {
+                    Console.WriteLine("ВЫ УГАДАЛИ!!!");
+                }
+                else if (userNumber < secretNumber)
+                {
+                    Console.WriteLine("Секретное число больше");
+                }
+                else
+                {
+                    Console.WriteLine("Секретное число меньше");
+
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
